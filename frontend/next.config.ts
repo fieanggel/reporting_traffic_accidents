2fba39a70e8a2775e93e011ed038fd800ad599f3
Raelqiansyah Putranta D <raelqiansyahputranta@gmail.com>
2026-04-24 13:24:25 +0700

    fix: update Dockerfile and next.config.ts for improved build process and compatibility

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,23 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. Wajib untuk Docker + Nginx
   output: 'export', 
-  
-  // 2. Agar gambar tidak error saat export statis
   images: {
     unoptimized: true,
   },
-
   experimental: {
     serverActions: {
       allowedOrigins: ["192.168.1.109:3000", "localhost:3000"],
     },
-    // 3. Matikan bentrok Turbopack vs Webpack
-    turbopack: {} 
   },
-  
-  // 4. Tetap pertahankan fallback fs jika memang dibutuhkan library-mu
+  // Tambahkan ini di tingkat paling luar (bukan di dalam experimental)
+  // Ini trik untuk Next.js 16 agar tidak komplain soal Turbopack
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
